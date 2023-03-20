00833e266f8037b29a7446f911965d90b85c675d
Akshit Vaghasiya <akshitvaghasiya2003@gmail.com>
2023-03-20 10:46:06 +0530

    image in category and sub category

--- api/v1/web/category/save.js
+++ api/v1/web/category/save.js
@@ -2,41 +2,77 @@ import { Joi } from '../../../../utilities/schemaValidate'
 import { Router } from 'express';
 import commonResolver from '../../../../utilities/commonResolver'
 import { addCategory } from "../../../../services/category/category";
+import { decodeJwtTokenFn } from "../../../../utilities/universal";
+
 const router = new Router();
+
+let multer = require("multer");
+
+let storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './views/categoryImages');
+  },
+  filename: function (req, file, cb) {
+    const ext = file.mimetype.split("/")[1];
+    cb(null, `${file.fieldname}-${Date.now()}.${ext}`);
+    // cb(null, file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/jpg" || file.mimetype === "image/png") {
+    cb(null, true);
+  } else {
+    // cb(new Error("File format should be CSV"), false); // if validation failed then generate error
+    return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
+  }
+};
+let upload = multer({ storage: storage, fileFilter: fileFilter });
 
 /**
  * @swagger
- * /api/v1/customer/add:
+ * /api/v1/category/add:
  *  post:
- *   tags: ["Customer"]
- *   summary: Save customer information.
- *   description: api used for Save customer information.
+ *   tags: ["Category"]
+ *   summary: Save category information.
+ *   description: api used for Save category information.
+ *   consumes:
+ *      - application/x-www-form-urlencoded
  *   parameters:
- *      - in: body
- *        name: lead
- *        description: Save customer information.
- *        schema:
- *         type: object
- *         properties:
- *           categoryName:
- *             type: string
- *           image:
- *             type: string
- *           description:
- *             type: string
+ *      - name: categoryName
+ *        description: enter category Name.
+ *        in: formData
+ *        type: string
+ *      - name: image
+ *        description: save category image information.
+ *        in: formData
+ *        type: file
+ *      - name: description
+ *        description: enter description.
+ *        in: formData
+ *        type: string
  *   responses:
  *    "200":
  *     description: success
  *    "400":
  *     description: fail
+ *   security:
+ *      - bearerAuth: [] 
  */
 
 const dataSchema = Joi.object({
   categoryName: Joi.string().required().label("categoryName"),
-  image: Joi.string().required().label("image"),
+  // image: Joi.string().required().label("image"),
   description: Joi.string().required().label("description"),
 });
 
-router.post('/add', commonResolver.bind({ modelService: addCategory, isRequestValidateRequired: true, schemaValidate: dataSchema }))
+router.post('/add',
+  decodeJwtTokenFn,
+  upload.single('image'),
+  commonResolver.bind({
+    modelService: addCategory,
+    isRequestValidateRequired: true,
+    schemaValidate: dataSchema
+  }))
 
 export default router;
--- api/v1/web/subcategory/save.js
+++ api/v1/web/subcategory/save.js
@@ -2,42 +2,81 @@ import { Joi } from '../../../../utilities/schemaValidate'
 import { Router } from 'express';
 import commonResolver from '../../../../utilities/commonResolver'
 import { addSubCategory } from "../../../../services/subcategory/subcategory";
+import { decodeJwtTokenFn } from "../../../../utilities/universal";
+
 const router = new Router();
+
+let multer = require("multer");
+
+let storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './views/subCategoryImages');
+  },
+  filename: function (req, file, cb) {
+    const ext = file.mimetype.split("/")[1];
+    cb(null, `${file.fieldname}-${Date.now()}.${ext}`);
+    // cb(null, file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/jpg" || file.mimetype === "image/png") {
+    cb(null, true);
+  } else {
+    // cb(new Error("File format should be CSV"), false); // if validation failed then generate error
+    return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
+  }
+};
+let upload = multer({ storage: storage, fileFilter: fileFilter });
 
 /**
  * @swagger
- * /api/v1/customer/add:
+ * /api/v1/subcategory/add:
  *  post:
- *   tags: ["Customer"]
- *   summary: Save customer information.
- *   description: api used for Save customer information.
+ *   tags: ["SubCategory"]
+ *   summary: Save Sub category information.
+ *   description: api used for Save Sub category information.
+ *   consumes:
+ *      - application/x-www-form-urlencoded
  *   parameters:
- *      - in: body
- *        name: lead
- *        description: Save customer information.
- *        schema:
- *         type: object
- *         properties:
- *           categoryName:
- *             type: string
- *           image:
- *             type: string
- *           description:
- *             type: string
+ *      - name: categoryId
+ *        description: enter sub category Id.
+ *        in: formData
+ *        type: string
+ *      - name: subCategoryName
+ *        description: enter sub Category Name.
+ *        in: formData
+ *        type: string
+ *      - name: image
+ *        description: save category image information.
+ *        in: formData
+ *        type: file
+ *      - name: description
+ *        description: enter description.
+ *        in: formData
+ *        type: string
  *   responses:
  *    "200":
  *     description: success
  *    "400":
  *     description: fail
+ *   security:
+ *      - bearerAuth: [] 
  */
 
 const dataSchema = Joi.object({
-  subCategoryName: Joi.string().required().label("subCategoryName"),
   categoryId: Joi.string().required().label("categoryId"),
-  image: Joi.string().required().label("image"),
+  subCategoryName: Joi.string().required().label("subCategoryName"),
+  // image: Joi.string().required().label("image"),
   description: Joi.string().required().label("description")
 });
 
-router.post('/add', commonResolver.bind({ modelService: addSubCategory, isRequestValidateRequired: true, schemaValidate: dataSchema }))
-
+router.post('/add',
+  decodeJwtTokenFn,
+  upload.single('image'),
+  commonResolver.bind({
+    modelService: addSubCategory,
+    isRequestValidateRequired: true,
+    schemaValidate: dataSchema
+  }))
 export default router;
